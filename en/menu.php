af93478d2cc598cc129d365ef433ad2c5ab0d514
julio.quiroga <quirogajulio27@gmail.com>
2018-01-20 23:10:35 -0300

    correccion de ruta a contacto

--- en/menu.php
+++ en/menu.php
@@ -5,14 +5,14 @@
             <a href="galery.php">Products</a>
             <ul>
                 <li>
-                    <a href="galery.php?est=6">Spring-Summer &hellip;</a>
+                    <a href="galery.php?est=6">Spring-Summer</a>
                     <ul>
                         <li><a href="galery.php?est=6&cat=4">Purse</a></li>
                         <li><a href="galery.php?est=6&cat=5">Backpacks</a></li>
                     </ul>
                 </li>
                 <li>
-                    <a href="galery.php?est=4">Autumn-Winter &hellip;</a>
+                    <a href="galery.php?est=4">Autumn-Winter</a>
                     <ul>
                         <li><a href="galery.php?est=4&cat=4">Purse</a></li>
                         <li><a href="galery.php?est=4&cat=5">Backpacks</a></li>
@@ -23,21 +23,21 @@
         <li>
             <a href="galery.php?acces=0">Accesories</a>
             <ul>
-                <li><a href="galery.php?acces=2">Wallet &hellip;</a></li>
-                <li><a href="galery.php?acces=3">Belts &hellip;</a></li>
-                <li><a href="galery.php?acces=4">Merchandising/Gifts &hellip;</a></li>
+                <li><a href="galery.php?acces=2">Wallet</a></li>
+                <li><a href="galery.php?acces=3">Belts</a></li>
+                <li><a href="galery.php?acces=4">Merchandising/Gifts</a></li>
             </ul>
         </li>
         <li>
             <a href="answer.php">Information</a>
             <ul>
-                <li><a href="answer.php#cambios">Leather Care &hellip;</a></li>
-                <li><a href="answer.php#productos">Damaged Products &hellip;</a></li>
-                <li><a href="answer.php#compra">Your purchase &hellip;</a></li>
+                <li><a href="answer.php#cambios">Leather Care</a></li>
+                <li><a href="answer.php#productos">Damaged Products</a></li>
+                <li><a href="answer.php#compra">Your purchase</a></li>
             </ul>
         </li>
-        <li><a href="index.php#formContact">Contacts</a></li>
-        <li><a href="../index.php">Idioma<img class="imgRedonda" src="iconoBandera/es-bandera.png" alt="/ES"></a></li>
+        <li><a href="index.php#formContact">Contact</a></li>
+        <li><a href="../index.php">Language<img class="imgRedonda" src="iconoBandera/es-bandera.png" alt="/ES"></a></li>
 
     </ul>
 </nav>
